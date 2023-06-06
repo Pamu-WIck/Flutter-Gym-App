c7c58663ae8003bae0cdc479bdb88c6b9d97023b
Pamu-Wick <pamudithawm@gmail.com>
2023-06-06 17:45:39 +0530

    get data from meals collection

--- lib/UI/dailyActivity.dart
+++ lib/UI/dailyActivity.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:jail_fitness/res/colors.dart';
-
+import 'package:jail_fitness/auth.dart';
+import 'package:jail_fitness/util/MealService.dart'; // Import MealService
 import 'home_page.dart';
-
 
 class DailyRoutine extends StatefulWidget {
   const DailyRoutine({Key? key}) : super(key: key);
@@ -12,188 +13,99 @@ class DailyRoutine extends StatefulWidget {
 }
 
 class _DailyRoutineState extends State<DailyRoutine> {
+  final User? user = Auth().currentUser;
+  final String userID = Auth().currentUser!.uid;
+  final MealService mealService = MealService(); // Initialize MealService
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: Column(
-        children: [
-          //back button and title
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: mealService.getMealForUser(userID),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // Show loading spinner while waiting for data
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}'); // Show error if there is any
+          } else if (snapshot.hasData) {
+            return Column(
               children: [
-                //back button
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomePage()));
-                  },
-                  icon: Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.white,
-                    size: 20,
-                  ),
-                ),
-
-                //title
-                Text(
-                  'Daily Activity',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                //empty space
-                SizedBox(width: 50),
-              ],
-            ),
-          ),
-
-          //empty space
-          SizedBox(height: 20),
-
-          //text
-          Column(
-            children: [
-              Text(
-                'Today',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'You have 2 hours left',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-
-          //space between text and progress bar
-          SizedBox(height: 20),
-          //meal plan with meal icon
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
+                SizedBox(height: 20),
+                Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    //meal plan
-                    Text(
-                      'Meal Plan',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Meal Plan',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          buildMealRow('Breakfast', snapshot.data!['breakfast']),
+                          SizedBox(height: 10),
+                          buildMealRow('Lunch', snapshot.data!['lunch']),
+                          SizedBox(height: 10),
+                          buildMealRow('Dinner', snapshot.data!['dinner']),
+                        ],
                       ),
                     ),
-
-                    Row(
-                      children: [
-                        //#282D3B color round edge box
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xff282D3B),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Icon(
-                            Icons.fastfood,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          'Breakfast',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
                     SizedBox(height: 10),
-                    Row(
-                      children: [
-                        //#282D3B color round edge box
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xff282D3B),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Icon(
-                            Icons.fastfood,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          'Lunch',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        //#282D3B color round edge box
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xff282D3B),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Icon(
-                            Icons.fastfood,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          'Dinner',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 ),
-              ),
-              SizedBox(height: 10),
-              //progress bar
-
-            ],
-          ),
-
-
-
-        ],
+              ],
+            );
+          } else {
+            return Text('No meal data found for the user'); // Show a message when no data is found
+          }
+        },
       ),
+    );
+  }
+
+  Widget buildMealRow(String mealType, String mealDetail) {
+    return Row(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: Color(0xff282D3B),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Icon(
+            Icons.fastfood,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              mealType,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              mealDetail,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/util/MealService.dart
+++ lib/util/MealService.dart
@@ -3,7 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class MealService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> addMeal(String customerId, String breakfast, String lunch, String dinner) async {
+  Future<void> addMeal(String customerId, String breakfast, String lunch,
+      String dinner) async {
     try {
       await _firestore.collection('meals').add({
         'customerId': customerId,
@@ -13,6 +14,25 @@ class MealService {
       });
     } catch (e) {
       print('Error adding meal: $e');
+      throw e;
+    }
+  }
+
+  Future<Map<String, dynamic>> getMealForUser(String customerId) async {
+    try {
+      QuerySnapshot mealSnapshot = await _firestore
+          .collection('meals')
+          .where('customerId', isEqualTo: customerId)
+          .limit(1) // only get the first document
+          .get();
+
+      if (mealSnapshot.docs.isEmpty) {
+        return {}; // return an empty map if no meals found
+      } else {
+        return mealSnapshot.docs.first.data() as Map<String, dynamic>;
+      }
+    } catch (e) {
+      print('Error getting meals: $e');
       throw e;
     }
   }
